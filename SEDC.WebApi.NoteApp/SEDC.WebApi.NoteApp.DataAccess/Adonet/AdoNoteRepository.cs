75ab5f2fd13d1a4ab53fc5b70b0e716cd302481d
risteskidimitar <60626102+risteskidimitar@users.noreply.github.com>
2020-10-12 22:54:16 +0200

    hw

---
+++ SEDC.WebApi.NoteApp/SEDC.WebApi.NoteApp.DataAccess/Adonet/AdoNoteRepository.cs
@@ -0,0 +1,111 @@
+﻿using SEDC.WebApi.NoteApp.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace SEDC.WebApi.NoteApp.DataAccess.Adonet
+{
+    public class AdoNoteRepository : IRepository<Note>
+    {
+        private readonly string _connectionString;
+        public AdoNoteRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IEnumerable<Note> GetAll()
+        {
+            SqlConnection connection = new SqlConnection(_connectionString);
+            connection.Open();
+            SqlCommand command = new SqlCommand();
+
+            command.Connection = connection;
+            command.CommandText = "SELECT * FROM dbo.[Notes]";
+            SqlDataReader dataReader = command.ExecuteReader();
+
+            List<Note> notes = new List<Note>();
+
+            while (dataReader.Read())
+            {
+                var note = new Note
+                {
+                    Id = dataReader.GetFieldValue<int>(0),
+                    Text = dataReader.GetFieldValue<string>(1),
+                    Color = dataReader.GetFieldValue<string>(2),
+                    Tag = dataReader.GetFieldValue<int>(3),
+                    UserId = dataReader.GetFieldValue<int>(4),
+                    User = dataReader.GetFieldValue<User>(5)
+                };
+
+                notes.Add(note);
+            }
+
+            connection.Close();
+
+            return notes;
+        }
+
+        public void Insert(Note entity)
+        {
+            SqlConnection connection = new SqlConnection(_connectionString);
+            connection.Open();
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.Connection = connection;
+
+            cmd.CommandText =
+                         $"INSERT INTO dbo.[Notes] (Text, Color, Tag, UserId)" +
+                         $" VALUES(@noteText, @noteColor, @noteTag, @noteUserId)";
+            cmd.Parameters.AddWithValue("@noteText", entity.Text);
+            cmd.Parameters.AddWithValue("@noteColor", entity.Color);
+            cmd.Parameters.AddWithValue("@noteTag", entity.Tag);
+            cmd.Parameters.AddWithValue("@noteUserId", entity.UserId);
+
+            cmd.ExecuteNonQuery();
+            connection.Close();
+
+        }
+
+        public void Remove(Note entity)
+        {
+            SqlConnection connection =
+               new SqlConnection(_connectionString);
+            connection.Open();
+
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.Connection = connection;
+
+            cmd.CommandText = "DELETE FROM dbo.[Notes] WHERE Id = @id";
+            cmd.Parameters.AddWithValue("@id", entity.Id);
+
+            cmd.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        public void Update(Note entity)
+        {
+            SqlConnection connection =
+                new SqlConnection(_connectionString);
+
+            connection.Open();
+
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.Connection = connection;
+            cmd.CommandText = "UPDATE dbo.[Notes] " +
+                "SET Text = @noteText, Color = @noteColor, Tag = @noteTag, UserId = @noteUserId" +
+                "WHERE Id = @id";
+            cmd.Parameters.AddWithValue("@noteText", entity.Text);
+            cmd.Parameters.AddWithValue("@noteColor", entity.Color);
+            cmd.Parameters.AddWithValue("@noteTag", entity.Tag);
+            cmd.Parameters.AddWithValue("@noteUserId", entity.UserId);
+            cmd.Parameters.AddWithValue("@id", entity.Id);
+
+            cmd.ExecuteNonQuery();
+
+            connection.Close();
+        }
+    }
+}
---
+++ SEDC.WebApi.NoteApp/SEDC.WebApi.NoteApp.DataAccess/Dapper/DapperNoteRepository.cs
@@ -0,0 +1,62 @@
+﻿using Dapper;
+using Dapper.Contrib.Extensions;
+using SEDC.WebApi.NoteApp.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace SEDC.WebApi.NoteApp.DataAccess.Dapper
+{
+    public class DapperNoteRepository : IRepository<Note>
+    {
+        private readonly string _connectionString;
+
+        public DapperNoteRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IEnumerable<Note> GetAll()
+        {
+            using(var connection =
+                new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                var notes = connection
+                    .Query<Note>("SELECT * FROM dbo.[Notes]")
+                    .ToList();
+
+                return notes;
+            }
+        }
+
+        public void Insert(Note entity)
+        {
+            SqlConnection connection = new SqlConnection(_connectionString);
+            connection.Open();
+            connection.Insert(entity);
+            connection.Close();
+        }
+
+        public void Remove(Note entity)
+        {
+            using(var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                connection.Delete(entity);
+            }
+        }
+
+        public void Update(Note entity)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                connection.Update(entity);
+            }
+        }
+    }
+}
